55c95957f7db8613c8ad6e2e69d3f909dd90ffa7
Ro <epictetuse@gmail.com>
2023-11-03 19:37:13 +0300

    initial

---
+++ lib/api_endpoints.dart
@@ -0,0 +1,5 @@
+abstract class ApiEndpoints {
+  static const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+  static const postsComments =
+      'https://jsonplaceholder.typicode.com/posts/1/comments';
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PostsPage(),
+    );
+  }
+}
+
+class PostsPage extends StatefulWidget {
+  const PostsPage({super.key});
+
+  @override
+  State<PostsPage> createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('1232131'),
+      ),
+    );
+  }
+}
